13b640740aeda4faa10c7a1a1c6b0431108ef892
stoopr <robert.j.stoop@gmail.com>
2025-12-26 09:51:59 -0500

    Update hosting.py

--- MLOps/hosting/hosting.py
+++ MLOps/hosting/hosting.py
@@ -4,7 +4,7 @@ import os
 api = HfApi(token=os.getenv("HF_TOKEN"))
 api.upload_folder(
     folder_path="MLOps/deployment",
-    repo_id="RStoopAi/PIMA_Diabetes_Prediction"                                         
+    repo_id="RStoopAi/PIMA_Diabetes_Prediction",                                         
     repo_type="space",
     path_in_repo="",                          
 )
