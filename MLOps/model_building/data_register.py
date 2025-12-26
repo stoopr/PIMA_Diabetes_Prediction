0b39c718043fad5eaa1d33b1e99dcd42a3ef5b8b
stoopr <robert.j.stoop@gmail.com>
2025-12-26 09:47:02 -0500

    Update data_register.py

--- MLOps/model_building/data_register.py
+++ MLOps/model_building/data_register.py
@@ -10,7 +10,7 @@ api = HfApi(token=os.getenv("HF_TOKEN"))
 try:
     api.repo_info(repo_id=repo_id, repo_type=repo_type)
     print(f"Space '{repo_id}' already exists. Using it.")
-except RespositoryNotFoundError:
+except RepositoryNotFoundError:
     print(f"Space '{repo_id}' not found. Creating new space...")
     create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
     print(f"Space '{repo_id}' created.")
